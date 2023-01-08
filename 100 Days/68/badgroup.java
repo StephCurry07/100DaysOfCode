8f07559452d6846d3ad83600f86a7f2838738253
Steph <apoorv.gupta65@gmail.com>
2023-01-08 09:49:36 +0530

    modified:   100 Days/68/badgroup.java

--- 100 Days/68/badgroup.java
+++ 100 Days/68/badgroup.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
 import java.util.Arrays;
+
 public class badgroup{
+    static boolean isEven(int[] arr){
+        int a = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == 1){
+                return true;
+            }
+            if(arr[i]%2 != 0 && arr[i] % a != 0)
+                return true;
+        }
+        return false;
+    }
     public static void main(String[] args) {
         int n;
         Scanner sc = new Scanner(System.in);
@@ -9,5 +21,22 @@ public class badgroup{
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
+        Arrays.sort(arr);
+        int flag = 0;
+        if(isEven(arr)){
+            System.out.println(true);
+            return;
+        }
+        int a = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(a%arr[i] == 0 || arr[i]%a == 0){
+                continue;
+            }
+            else{
+                System.out.print(true);
+                return;
+            }
+        }
+        System.out.print(false);
     }
 }
