1906b3490764f115cea48eadd2a8ef34303dd7d5
Steph <apoorv.gupta65@gmail.com>
2023-01-08 06:29:24 +0530

    new file:   100 Days/68/badgroup.java

---
+++ 100 Days/68/badgroup.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class badgroup{
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+    }
+}
